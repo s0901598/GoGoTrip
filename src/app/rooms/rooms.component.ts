f2b1d2b520de1f76f167cabf02c398680d0e1d74
s0901598 <s0901598@gmail.com>
2025-04-09 13:39:04 +0800

    update img change

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -11,6 +11,21 @@ export class RoomsComponent implements OnInit {
 
   backgroundImage: string = '';
 
+  // 定義每個房間的照片陣列
+  roomImages = [
+    ['/assets/room2.jpg', '/assets/room3.jpg', '/assets/room4.jpg'],
+    ['/assets/room3.jpg', '/assets/room1.jpg', '/assets/room2.jpg'],
+    ['/assets/room4.jpg', '/assets/room2.jpg', '/assets/room1.jpg'],
+    ['/assets/room2.jpg', '/assets/room3.jpg', '/assets/room4.jpg'],
+    ['/assets/room3.jpg', '/assets/room1.jpg', '/assets/room2.jpg'],
+    ['/assets/room2.jpg', '/assets/room3.jpg', '/assets/room4.jpg'],
+    ['/assets/room4.jpg', '/assets/room2.jpg', '/assets/room1.jpg'],
+    ['/assets/room2.jpg', '/assets/room3.jpg', '/assets/room4.jpg']
+
+  ];
+  // 追蹤當前顯示的照片索引
+  currentImageIndices: number[] = [0, 0, 0, 0,0,0,0,0];
+
   constructor(private route: ActivatedRoute,private notifyService:BackgroundImageServiceService) {}
 
   ngOnInit() {
@@ -20,6 +35,20 @@ export class RoomsComponent implements OnInit {
       this.notifyService.Notify(bgImg);  // do something with id
     });
 
+  }
+
+  // 切換到上一張照片
+  prevImage(roomIndex: number) {
+    this.currentImageIndices[roomIndex] = 
+      (this.currentImageIndices[roomIndex] - 1 + this.roomImages[roomIndex].length) % 
+      this.roomImages[roomIndex].length;
+  }
+
+  // 切換到下一張照片
+  nextImage(roomIndex: number) {
+    this.currentImageIndices[roomIndex] = 
+      (this.currentImageIndices[roomIndex] + 1) % 
+      this.roomImages[roomIndex].length;
   }
 
 }
